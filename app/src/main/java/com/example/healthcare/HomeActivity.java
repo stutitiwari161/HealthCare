be2a63e8d6110ad320c70bb96fe534588f7d912e
Stuti <stutitiwari161@gmail.com>
2023-07-17 23:53:57 +0530

    1st Commit

---
+++ app/src/main/java/com/example/healthcare/HomeActivity.java
@@ -0,0 +1,43 @@
+ package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+ public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedPreferences= getSharedPreferences("shared_prefer", Context.MODE_PRIVATE);
+        String username=sharedPreferences.getString("username","").toString();
+        Toast.makeText(getApplicationContext(), "Welcome"+username, Toast.LENGTH_SHORT).show();
+
+        CardView exit =findViewById(R.id.cardEXIT);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              SharedPreferences.Editor editor=sharedPreferences.edit();
+              editor.clear();
+              editor.apply();
+              startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+            }
+        });
+
+        CardView findDoctor= findViewById(R.id.cardFindDoctor);
+        findDoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,FindDoctorActivity.class));
+            }
+        });
+    }
+}
